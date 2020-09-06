e510d29485e8388453bcaf82a617627ae221ea8d
unknown <thefray.br@gmail.com>
2020-09-05 22:02:17 -0300

    new parts of menu

--- desafiofinal.js
+++ desafiofinal.js
@@ -6,26 +6,77 @@ var menu = readline.createInterface({
     output: process.stdout
 });
 
-menuOptions();
+mainMenu();
+
+function mainMenu() {
+    menu.question("1 - Cadastrar funcionário \n2 - Imprimir contracheque \nEXIT - Sair\n", function (option) {
+        var opcao = option
+        switch (opcao) {
+            case "1":
+                menuOptions();
+                break;
+            case "2":
+                printPayCheck();
+                break;
+            case "exit":
+                menu.close();
+        }
+    })
+}
+
+
 
 var funcionarios = []
 
 function menuOptions() {
 
     menu.question("Digite o nome do funcionário \n", function (nome) {
-        menu.question("Digite o salario do funcionário \n", function (numero) {
-            funcionarios.push({ "NOME": nome, "SALARIO": numero })
+        menu.question("Digite o salario do funcionário \n", function (salario) {
+            funcionarios.push({ "NOME": nome, "SALARIO": salario })
             console.log(funcionarios)
             menu.question("Deseja adicionar outro funcionário? (S/N) \n", function (resp) {
-                var confirmation = resp;
-                if (confirmation == "s") {
-                    menuOptions();
-                } menu.close();
+                var confirmation = resp
+                switch (confirmation) {
+                    case "s":
+                        menuOptions();
+                        break;
+                    case "n":
+                        mainMenu();
+                        break;
+                }
+
             })
 
         })
 
     })
-    
+
 }
 
+
+
+var codigo = 0
+
+function printPayCheck() {
+    menu.question("Digite o código do funcionário: \n", function (index) {
+        codigo = parseFloat(index)
+        console.log(funcionarios[codigo])
+        calculoINSS();
+
+        
+
+    })
+}
+
+
+function calculoINSS() {
+    
+    if (funcionarios[codigo].salario <= 1045.00){
+        console.log("salario mínimo")
+    } else {
+        mainMenu();
+    }
+
+ 
+    
+}
