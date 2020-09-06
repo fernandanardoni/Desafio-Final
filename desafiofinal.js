8efc1a0212d50e24cf36ab0248843c176fdff57a
unknown <thefray.br@gmail.com>
2020-09-06 00:25:10 -0300

    error?

--- desafiofinal.js
+++ desafiofinal.js
@@ -9,13 +9,16 @@ var menu = readline.createInterface({
 mainMenu();
 
 function mainMenu() {
-    menu.question("1 - Cadastrar funcionário \n2 - Imprimir contracheque \nEXIT - Sair\n", function (option) {
+    menu.question("1 - Cadastrar funcionário \n2 - Calcular impostos \n 3 - Imprimir contracheque \nEXIT - Sair\n", function (option) {
         var opcao = option
         switch (opcao) {
             case "1":
                 menuOptions();
                 break;
             case "2":
+                calculoINSS();
+                break;
+            case "3":
                 printPayCheck();
                 break;
             case "exit":
@@ -32,7 +35,7 @@ function menuOptions() {
 
     menu.question("Digite o nome do funcionário \n", function (nome) {
         menu.question("Digite o salario do funcionário \n", function (salario) {
-            funcionarios.push({ "NOME": nome, "SALARIO": salario })
+            funcionarios.push({ "NOME": nome, "SALARIO": parseFloat(salario)})
             console.log(funcionarios)
             menu.question("Deseja adicionar outro funcionário? (S/N) \n", function (resp) {
                 var confirmation = resp
@@ -54,29 +57,24 @@ function menuOptions() {
 }
 
 
+function calculoINSS() {
+    menu.question("Digite o código do funcionário: \n", function (index){
+        var codigo = parseInt(index)
+        console.log(funcionarios[codigo])
+        if (salario <= 1045.00){
+            console.log("Salário Mínimo")
+        } else {
+            console.log("Maior")
+        }
+    
+    }
 
-var codigo = 0
 
 function printPayCheck() {
     menu.question("Digite o código do funcionário: \n", function (index) {
-        codigo = parseFloat(index)
-        console.log(funcionarios[codigo])
-        calculoINSS();
-
+        var codigo = parseFloat(index)
         
+        console.log(funcionarios[codigo])
 
     })
-}
-
-
-function calculoINSS() {
-    
-    if (funcionarios[codigo].salario <= 1045.00){
-        console.log("salario mínimo")
-    } else {
-        mainMenu();
-    }
-
- 
-    
 }
