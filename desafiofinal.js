37e0840589294e2fe19913b65ffd37820f4b79e1
unknown <thefray.br@gmail.com>
2020-09-06 01:17:04 -0300

    modifying calculator

--- desafiofinal.js
+++ desafiofinal.js
@@ -16,13 +16,11 @@ function mainMenu() {
                 menuOptions();
                 break;
             case "2":
-                calculoINSS();
-                break;
-            case "3":
                 printPayCheck();
                 break;
             case "exit":
                 menu.close();
+
         }
     })
 }
@@ -31,21 +29,34 @@ function mainMenu() {
 
 var funcionarios = []
 
+var payment
+var finalPayment
+
+function firstCase() {
+    var inss = payment * (7.5 / 100)
+    finalPayment = (payment - inss).toFixed(2)
+
+}
+
+var aliquota1 = 1045.00; 
 
 function menuOptions() {
 
     menu.question("Digite o nome do funcionário \n", function (nome) {
+
         menu.question("Digite o salario do funcionário \n", function (salario) {
-            var payment = parseFloat(salario)
-            
-            if (payment <= 1045.00){
-                var inss = payment * (7.5/100)
-                var finalPayment = (payment - inss).toFixed(2)
-                funcionarios.push({ NOME: nome, BRUTO: payment.toFixed(2), LIQUIDO: finalPayment })
-                console.log(funcionarios)    
-            } else {
-                console.log("Maior")
+            payment = parseFloat(salario)
+
+            switch (payment) {
+                case aliquota1:
+                    firstCase();
+                    break;
+                case payment > 1045.00:
+                    mainMenu();
             }
+            funcionarios.push({ NOME: nome, BRUTO: payment.toFixed(2), LIQUIDO: finalPayment })
+            console.log(funcionarios)
+
             menu.question("Deseja adicionar outro funcionário? (S/N) \n", function (resp) {
                 var confirmation = resp
                 switch (confirmation) {
@@ -71,7 +82,7 @@ function menuOptions() {
 function printPayCheck() {
     menu.question("Digite o código do funcionário: \n", function (index) {
         var codigo = parseFloat(index)
-        
+
         console.log(funcionarios[codigo])
 
     })
